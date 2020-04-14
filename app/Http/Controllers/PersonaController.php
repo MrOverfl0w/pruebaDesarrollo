2c5285a3fea3b2b9c1f4aa94271afcf5dc9cb8ef
Alberto Pumarejo <alberto_pumarejo@hotmail.com>
2020-04-14 11:34:00 -0500

    Controladores hechos

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Persona;
+
+class PersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $personas = Persona::all();
+        return $personas;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'identificacion'=>'required',
+            'nombre'=>'required',
+            'apellidos'=>'required',
+            'nacimiento'=>'required',
+            'profesion'=>'required',
+            'casado'=>'required',
+            'ingresos'=>'required'
+        ]);
+
+        $persona = new Persona();
+        $persona->identificacion = $request->identificacion;
+        $persona->nombre = $request->nombre;
+        $persona->apellidos = $request->apellidos;
+        $persona->nacimiento = $request->nacimiento;
+        $persona->profesion = $request->profesion;
+        $persona->casado = $request->casado;
+        $persona->ingresos = $request->ingresos;
+        $persona->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $persona = Persona::find($id);
+        return $persona;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $persona = Persona::findOrFail($id);
+        if (!is_null($request->nombre)){
+            $persona->nombre = $request->nombre;
+        }
+        if (!is_null($request->apellidos)){
+            $persona->apellidos = $request->apellidos;
+        }
+        if (!is_null($request->nacimiento)){
+            $persona->nacimiento = $request->nacimiento;
+        }
+        if (!is_null($request->profesion)){
+            $persona->profesion = $request->profesion;
+        }
+        if (!is_null($request->casado)){
+            $persona->casado = $request->casado;
+        }
+        if (!is_null($request->ingresos)){
+            $persona->ingresos = $request->ingresos;
+        }
+
+        $vehiculo = $request->vehiculo;
+        if (!is_null($vehiculo)){
+            $new = $persona->vehiculos()->where('idvehiculo',$vehiculo)->get()->first();
+            if (!is_null($new)){
+                $new->pivot->activo = 1;
+                $new->pivot->save();
+            }else{
+                $persona->vehiculos()->attach($vehiculo);
+            }
+        }
+
+        $persona->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $persona = Persona::find($id);
+        if ($persona){
+            $destroy = Persona::destroy($id);
+        }
+        if ($destroy){
+            return response('Success', 200);
+        }
+        return response('Failed', 404);
+    }
+}
---
+++ app/Http/Controllers/VehiculoController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Vehiculo;
+
+class VehiculoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $vehiculos = Vehiculo::all();
+        return $vehiculos;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'placa'=>'required',
+            'marca'=>'required',
+            'modelo'=>'required',
+            'puertas'=>'required',
+            'tipo'=>'required'
+        ]);
+
+        $vehiculo = new Vehiculo();
+        $vehiculo->placa = $request->placa;
+        $vehiculo->marca = $request->marca;
+        $vehiculo->modelo = $request->modelo;
+        $vehiculo->puertas = $request->puertas;
+        $vehiculo->tipo = $request->tipo;
+        $vehiculo->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $vehiculo = Vehiculo::find($id);
+        return $vehiculo;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $vehiculo = Vehiculo::findOrFail($id);
+        if (!is_null($request->placa)){
+            $vehiculo->placa = $request->placa;
+        }
+        if (!is_null($request->marca)){
+            $vehiculo->marca = $request->marca;
+        }
+        if (!is_null($request->modelo)){
+            $vehiculo->modelo = $request->modelo;
+        }
+        if (!is_null($request->puertas)){
+            $vehiculo->puertas = $request->puertas;
+        }
+        if (!is_null($request->tipo)){
+            $vehiculo->tipo = $request->tipo;
+        }
+        $vehiculo->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $vehiculo = Vehiculo::find($id);
+        if ($vehiculo){
+            $destroy = Vehiculo::destroy($id);
+        }
+        if ($destroy){
+            return response('Success', 200);
+        }
+        return response('Failed', 404);
+    }
+
+    public function desactivar($request){
+        $vehiculo = Vehiculo::find($request->id)->personas()->where('activo',1)->get()->first();
+        if (!is_null($vehiculo)){
+            $vehiculo->pivot->activo = 0;
+            $vehiculo->pivot->save();
+        }
+    }
+}
